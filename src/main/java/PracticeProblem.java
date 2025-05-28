d2b6f070e53c30dc0181a02c498a1fc85835e7f4
kelly-fu-ics4u1-02-25 <kelly.fu@student.tdsb.on.ca>
2025-05-28 14:26:56 +0000

    UPDATE

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,27 +1,50 @@
+
+
+	import java.util.HashSet;
+	import java.util.Set;
+
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+   public static void main(String args[]) {
 
-	}
+   }
 
-	public static void q1() {
-		//Write question 1 code here
-	}
+   public static int[] recaman(int n) {
+       if (n <= 0){
+       return new int[0];
+       }
 
-	public static void q2() {
-		//Write question 2 code here
-	}
+       Set<Integer> seen = new HashSet<>();
+       int[] result = new int[n];
+       result[0] = 1;
+       seen.add(1);
 
-	public static void q3() {
-		//Write question 3 code here
-	}
+       recamanHelper(result, seen, 1);
+       return result;
+   }
 
-	public static void q4() {
-		//Write question 4 code here
-	}
 
-	public static void q5() {
-		//Write question 5 code here
-	}
+   public static void recamanHelper(int[] result, Set<Integer> seen, int i){
+       if (i >= result.length){
+           return;
+       }
 
+       int previous = result[i - 1];
+       int after = previous - i - 1;
+       if (after > 0 && ! seen.contains(after)){
+           result[i] = after;
+       }
+       else{
+           result[i]= previous + i + 1;
+       }
+
+       seen.add(result[i]);
+       recamanHelper(result, seen, i + 1);
+      
+   }
 }
+
+
+
+
+	
